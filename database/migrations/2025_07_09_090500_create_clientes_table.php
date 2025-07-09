4457bee4664d60524548254586856ec8d9c50a1c
julian19G <julian19guerra@gmail.com>
2025-07-09 04:06:01 -0500

    Tabla clientes

---
+++ database/migrations/2025_07_09_090500_create_clientes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->string('telefono', 20)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->text('direccion')->nullable();
+            $table->timestamps(); // Crea created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
